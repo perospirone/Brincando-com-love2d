b2344d3f4ec63c93e069484d78b7649b0168102b
danitw <twz3xc21@gmail.com>
2020-09-29 17:08:40 -0300

    continuando estudando sobre cameras e fazendo um jogo pra dois jogadores

--- camera/main.lua
+++ camera/main.lua
@@ -1,8 +1,13 @@
 function love.load()
-  lume = require "lume"
-
-  player = {
+  player1 = {
     x = 100,
+    y = 100,
+    size = 25,
+    image = love.graphics.newImage("face.png")
+  }
+
+  player2 = {
+    x = 300,
     y = 100,
     size = 25,
     image = love.graphics.newImage("face.png")
@@ -10,54 +15,26 @@ function love.load()
 
   coins = {}
 
-  score = 0;
-  
-  if love.filesystem.getInfo("savedata.txt") then
-    file = love.filesystem.read("savedata.txt")
-    data = lume.deserialize(file)
-
-    --Apply the player info
-    player.x = data.player.x
-    player.y = data.player.y
-    player.size = data.player.size
-
-    for i,v in ipairs(data.coins) do
-      coins[i] = {
-        x = v.x,
-        y = v.y,
-        size = 10,
-        image = love.graphics.newImage("dollar.png")
-      }
-    end
-  else
-    -- Only execute this if you don't have a save file
-    for i=1,25 do
-      table.insert(coins,
-        {
-          x = love.math.random(50, 650),
-          y = love.math.random(50, 450),
-          size = 10,
-          image = love.graphics.newImage("dollar.png")
-        }
-        )
-    end
-    for i=#coins,1,-1 do
-      if checkCollision(player, coins[i]) then
-        table.remove(coins, i)
-        player.size = player.size + 1
-        score = score + 1
-      end
-    end
+  for i=1,25 do
+    table.insert(coins,
+    {
+      x = math.random(50, 650),
+      y = math.random(50, 450),
+      size = 10,
+      image = love.graphics.newImage("dollar.png")
+    }
+  )
   end
-  shakeDuration = 0
-  shakeWait = 0
-  shakeOffset = {x = 0, y = 0}
+
+  score1 = 0
+  score2 = 0
+
+  screenCanvas = love.graphics.newCanvas(400, 600)
 end
 
-function checkCollision(p1, p2) 
+function checkCollision(p1, p2)
   -- Calculating distance in 1 line
-  -- Subtract the x's and y's, square the difference
-  -- Sum the squares and find the root of the sum.
+  -- Subtract the x's and y's, square the difference, sum the squares and find the root of the sum.
   local distance = math.sqrt((p1.x - p2.x)^2 + (p1.y - p2.y)^2)
   -- Return whether the distance is lower than the sum of the sizes.
   return distance < p1.size + p2.size
@@ -84,62 +61,77 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown("left") then
-    player.x = player.x - 200 * dt
+    player1.x = player1.x - 200 * dt
   elseif love.keyboard.isDown("right") then
-    player.x = player.x + 200 * dt
+    player1.x = player1.x + 200 * dt
   end
 
   if love.keyboard.isDown("up") then
-    player.y = player.y - 200 * dt
+    player1.y = player1.y - 200 * dt
   elseif love.keyboard.isDown("down") then
-    player.y = player.y + 200 * dt
+    player1.y = player1.y + 200 * dt
   end
 
-  -- Start at the end, until 1, with steps of -1 
+  if love.keyboard.isDown("a") then
+    player2.x = player2.x - 200 * dt
+  elseif love.keyboard.isDown("d") then
+    player2.x = player2.x + 200 * dt
+  end
+
+  if love.keyboard.isDown("w") then
+    player2.y = player2.y - 200 * dt
+  elseif love.keyboard.isDown("s") then
+    player2.y = player2.y + 200 * dt
+  end
+
   for i=#coins,1,-1 do
-    if checkCollision(player, coins[i]) then
+    if checkCollision(player1, coins[i]) then
       table.remove(coins, i)
-      player.size = player.size + 1
-      score = score + 1
-      shakeDuration = 0.3
+      player1.size = player1.size + 1
+      score1 = score1 + 1
+    elseif checkCollision(player2, coins[i]) then
+      table.remove(coins, i)
+      player2.size = player2.size + 1
+      score2 = score2 + 1
     end
   end
-
-  function love.keypressed(key)
-    if key == "f1" then
-      saveGame()
-    elseif key == "f2" then
-      love.filesystem.remove("savedata.txt")
-      love.event.quit("restart")
-    end
-  end
-  if shakeDuration > 0 then
-    shakeDuration = shakeDuration - dt
-    if shakeWait > 0 then
-      shakeWait = shakeWait - dt
-    else
-      shakeOffset.x = love.math.random(-5,5)
-      shakeOffset.y = love.math.random(-5,5)
-      shakeWait = 0.05
-    end
-  end
-
 end
 
 function love.draw()
+  love.graphics.setCanvas(screenCanvas)
+  love.graphics.clear()
+  drawGame(player1)
+  love.graphics.setCanvas()
+  love.graphics.draw(screenCanvas)
+
+  love.graphics.setCanvas(screenCanvas)
+  love.graphics.clear()
+  drawGame(player2)
+  love.graphics.setCanvas()
+  love.graphics.draw(screenCanvas, 400)
+
+  love.graphics.line(400, 0, 400, 600)
+
+  love.graphics.print("Player 1 - " .. score1, 10, 10)
+  love.graphics.print("Player 2 - " .. score2, 10, 30)
+end
+
+function drawGame(focus)
   love.graphics.push()
-  love.graphics.translate(-player.x + 400, -player.y + 300)
-  if shakeDuration > 0 then
-    love.graphics.translate(shakeOffset.x, shakeOffset.y)
-  end
-  love.graphics.circle("line", player.x, player.y, player.size)
-  love.graphics.draw(player.image, player.x, player.y,
-    0, 1, 1, player.image:getWidth()/2, player.image:getHeight()/2)
+  love.graphics.translate(-focus.x + 200, -focus.y + 300)
+
+  love.graphics.circle("line", player1.x, player1.y, player1.size)
+  love.graphics.draw(player1.image, player1.x, player1.y,
+  0, 1, 1, player1.image:getWidth()/2, player1.image:getHeight()/2)
+
+  love.graphics.circle("line", player2.x, player2.y, player2.size)
+  love.graphics.draw(player2.image, player2.x, player2.y,
+  0, 1, 1, player2.image:getWidth()/2, player2.image:getHeight()/2)
+
   for i,v in ipairs(coins) do
     love.graphics.circle("line", v.x, v.y, v.size)
     love.graphics.draw(v.image, v.x, v.y,
-      0, 1, 1, v.image:getWidth()/2, v.image:getHeight()/2)
+    0, 1, 1, v.image:getWidth()/2, v.image:getHeight()/2)
   end
   love.graphics.pop()
-  love.graphics.print(score, 10, 10)
 end
